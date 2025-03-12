60720341d6965af8b97cf412eed52f1f5ff473c9
CMcLeod21074 <21074@students.lynfield.school.nz>
2025-03-13 09:28:00 +1300

    Add files via upload

---
+++ demo_program V2.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Please type your first number"))
+num_2 = int(input("Please type your second number"))
+num_3 = int(input("Please type your third number"))
+
+add = num_1 + num_2 + num_3
+
+print("Your two numbers add to",add)
